840083a91d58314ca285fce9a7a20cebbf47a919
dibkb <dibas9110@gmail.com>
2023-11-02 18:17:58 +0530

    login redirect

--- src/app/login/layout.tsx
+++ src/app/login/layout.tsx
@@ -1,13 +1,18 @@
 "use client";
 import { useSession } from "next-auth/react";
+import { useRouter } from "next/navigation";
 import Image from "next/image";
+import { useEffect } from "react";
 export default function DashboardLayout({
   children, // will be a page or nested layout
 }: {
   children: React.ReactNode;
 }) {
-  const { data: session } = useSession();
-  console.log(session);
+  const { data: session, status } = useSession();
+  const router = useRouter();
+  useEffect(() => {
+    if (status === "authenticated") router.push("dashboard");
+  }, [router, status]);
   return (
     <section className="container mx-auto">
       {/* Include shared UI here e.g. a header or sidebar */}
