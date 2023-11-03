3a216935f2e4c60bd42bd5acc6e2d43b9121a33c
dibkb <dibas9110@gmail.com>
2023-11-04 00:41:47 +0530

    margin top

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ import Link from "next/link";
 export default function Home() {
   return (
     <main className="flex items-center w-[600px] mx-auto justify-center min-h-[600px] flex-col gap-6">
-      <h3 className="text-4xl mt-4">Traffic Management System</h3>
+      <h3 className="text-4xl mt-12">Traffic Management System</h3>
       <h3 className="text-2xl my-4">Group 7</h3>
       <Image
         src="https://upload.wikimedia.org/wikipedia/en/2/20/Indian_Institute_of_Information_Technology%2C_Lucknow_Logo.png"
