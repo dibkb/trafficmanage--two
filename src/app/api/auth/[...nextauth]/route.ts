c97bc061a595af8caf15a147531a7947105aa041
dibkb <dibas9110@gmail.com>
2023-11-03 16:34:04 +0530

    authoptions 2

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -17,21 +17,4 @@ export const authOptions = {
     },
   },
 };
-export const handler = NextAuth({
-  // Configure one or more authentication providers
-  providers: [
-    GoogleProvider({
-      clientId:
-        "277692744282-4berhn25p8u3a26125e6da1kimj0tteg.apps.googleusercontent.com" as string,
-      clientSecret: "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" as string,
-    }),
-    // ...add more providers here
-  ],
-  secret: process.env.NEXTAUTH_SECRET,
-  callbacks: {
-    async signIn(user: any) {
-      return true;
-    },
-  },
-});
-export { handler as GET, handler as POST };
+export default NextAuth(authOptions);
